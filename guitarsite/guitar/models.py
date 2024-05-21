c86bd8f2bb93fb84703c5a96cecb67f0826b4ed6
alexvic-che <alexvic.che@gmail.com>
2024-05-21 16:08:22 +0300

    Added Post model

--- guitarsite/guitar/models.py
+++ guitarsite/guitar/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    img = models.ImageField()
+    title = models.CharField(max_length=50)
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
