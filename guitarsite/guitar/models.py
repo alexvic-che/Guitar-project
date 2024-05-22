cf70ae71d1fc33f3c98495ee17ff89468fb5dc46
alexvic-che <alexvic.che@gmail.com>
2024-05-22 23:49:28 +0300

    make migrations first

--- guitarsite/guitar/models.py
+++ guitarsite/guitar/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 class Post(models.Model):
-    img = models.ImageField()
     title = models.CharField(max_length=50)
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
