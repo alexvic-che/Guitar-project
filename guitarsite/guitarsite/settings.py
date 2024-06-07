b6d8b767832daf39d5bc29cba92ba391701a5f24
alexvic-che <alexvic.che@gmail.com>
2024-06-07 20:27:32 +0300

    Added debug_toolbar

--- guitarsite/guitarsite/settings.py
+++ guitarsite/guitarsite/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 # Application definition
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    "debug_toolbar",
     'guitar',
 ]
 
@@ -49,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'guitarsite.urls'
--- guitarsite/guitarsite/urls.py
+++ guitarsite/guitarsite/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from guitar import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("__debug__/", include("debug_toolbar.urls")),
     path("", include("guitar.urls"))
 ]
 
