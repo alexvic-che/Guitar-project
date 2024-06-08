8f9e7aebb9b3068c333d897ca4295aef76a5e9b3
alexvic-che <alexvic.che@gmail.com>
2024-06-08 19:40:09 +0300

    added setting up the admin panel

--- guitarsite/guitar/admin.py
+++ guitarsite/guitar/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
-from .models import Sings
+from .models import Sings, Difficulty, Authors
 
-admin.site.register(Sings)
+
+@admin.register(Sings)
+class SingsAdmin(admin.ModelAdmin):
+    list_display = ["title","time_create","is_published","difficult","author"]
+    list_display_links = ["title"]
+    list_editable = ["is_published", "difficult" ,"author"]
+    list_per_page = 10
+
+@admin.register(Difficulty)
+class DifficultyAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_display_links = ["name"]
+    list_per_page = 10
+
+@admin.register(Authors)
+class AuthorsAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_display_links = ["name"]
+    list_per_page = 10
--- guitarsite/guitar/models.py
+++ guitarsite/guitar/models.py
@@ -6,19 +6,19 @@ class PublishedModel(models.Manager):
         return super().get_queryset().filter(is_published=Sings.Status.PUBLISHED)
 
 class Sings(models.Model):
-    class Status(models.TextChoices):
-        DRAFT = '0', 'Черновик'
-        PUBLISHED = '1', 'Опубликовано'
+    class Status(models.IntegerChoices):
+        DRAFT = 0, 'Черновик'
+        PUBLISHED = 1, 'Опубликовано'
 
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, verbose_name="Название песни")
     slug = models.SlugField(max_length=255, unique=True,db_index=True)
     content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
     time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    difficult = models.ForeignKey("Difficulty", on_delete=models.PROTECT, related_name="sings")
+    is_published = models.BooleanField(choices=tuple(map(lambda x:(bool(x[0]),x[1]),Status.choices)), default=Status.DRAFT, verbose_name="Публикация")
+    difficult = models.ForeignKey("Difficulty", on_delete=models.PROTECT, related_name="sings", verbose_name="Сложность")
     chords = models.ManyToManyField("Chords", related_name="sings")
-    author = models.ForeignKey("Authors", on_delete=models.PROTECT, related_name="sings")
+    author = models.ForeignKey("Authors", on_delete=models.PROTECT, related_name="sings", verbose_name="Автор")
 
     objects = models.Manager()
     published = PublishedModel()
@@ -40,25 +40,32 @@ class Sings(models.Model):
 
 
 class Difficulty(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Сложность")
     slug = models.SlugField(max_length=255, db_index=True, unique=True)
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name = "Сложность песни"
+        verbose_name_plural = "Сложность песен"
 
     def get_absolute_url(self):
         return reverse('sings_by_difficulty', kwargs={'difficult_slug':self.slug})
 
 class Chords(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Аккорды")
     slug = models.SlugField(max_length=255, db_index=True, unique=True)
 
     def __str__(self):
         return self.name
 
 class Authors(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Автор")
     slug = models.SlugField(max_length=255, db_index=True, unique=True)
+
+    class Meta:
+        verbose_name = "Автор песни"
+        verbose_name_plural = "Авторы песен"
 
     def __str__(self):
         return self.name
