a68294766e7efbf05b7d22b5e73264e84befb102
Aleksandr Cherevkov <alexvic.che@gmail.com>
2024-07-13 00:05:43 +0300

    add login and logout

--- guitarsite/users/urls.py
+++ guitarsite/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, re_path
+
+
 from . import views
+
 
 app_name = "users"
 
 urlpatterns = [
-    path("login/", views.login_user, name="login"),
+    path("login/", views.LoginUser.as_view(), name="login"),
     path("logout/", views.logout_user, name="logout"),
 ]
--- guitarsite/users/views.py
+++ guitarsite/users/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-def login_user(request):
-    return HttpResponse("Login")
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect, reverse
+from django.urls import reverse_lazy
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import logout, login, authenticate
+
+from .forms import LoginUserForm
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+
+
 def logout_user(request):
-    return HttpResponse("Logout")
+    logout(request)
+    return HttpResponseRedirect(reverse_lazy('index'))
